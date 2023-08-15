6ffc4e7517fb950d9125dacd9ac01eab03f42f47
Denis <zzomrot.ne@gmail.com>
2023-08-15 20:48:47 +0300

    Add coverage to ci

--- .solcover.js
+++ .solcover.js
@@ -4,7 +4,7 @@ module.exports = {
         yul: true,
         yulDetails: {
             optimizerSteps:
-                "dhfoDgvlfnTUtnIf" +               // None of these can make stack problems worse
+                "dhfoDgvlfnTUtnIf" +              // None of these can make stack problems worse
                 "[" +
                     "xa[r]EsLM" +                 // Turn into SSA and simplify
                     "CTUtTOntnfDIl" +             // Perform structural simplification
@@ -15,9 +15,9 @@ module.exports = {
 
                     "Tpel" +                       // Run functional expression inliner
                     "xa[rl]" +                     // Prune a bit more in SSA
-                    "xa[r]L" +                    // Turn into SSA again and simplify
+                    "xa[r]L" +                     // Turn into SSA again and simplify
                     "gvf" +                        // Run full inliner
-                    "CTUa[r]LSsTFOtfDna[r]Il" + // SSA plus simplify
+                    "CTUa[r]LSsTFOtfDna[r]Il" +    // SSA plus simplify
                 "]" +
                 "jml[jl] VTOl jml : fDnTO",
         },
